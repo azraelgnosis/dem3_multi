9aeefd088c5412474f207f487613787761070f22
azraelgnosis <azraelgnosis@gmail.com>
2020-03-31 02:05:35 -0400

    Move username validation client-side

--- dem3_multi/auth.py
+++ dem3_multi/auth.py
@@ -2,15 +2,29 @@ from flask import (
     Blueprint, render_template, request
 )
 
+from dem3_multi.data import get_db, get_usernames
+
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/register/', methods=('GET', 'POST'))
 def register():
+    usernames = get_usernames()
+
     if request.method == 'POST':
         username = request.form['username']
-        password = request.form['password']
+        password = request.form['password']        
 
-    return render_template('auth.html') # return render_template('auth/register.html')
+    return render_template('auth.html', usernames=usernames)
+
+def username_exists(username:str) -> bool:
+    db = get_db()
+
+    sql_check_player = "SELECT * FROM players" \
+        "WHERE username == ?"
+    
+    return bool(db.execute(sql_check_player, username))
+        
+
 
 @bp.route('/login/', methods=('GET', 'POST'))
 def login():
