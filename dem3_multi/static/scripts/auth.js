e2866ddff558cbb908c019edc1a2c6d8fad38834
azraelgnosis <azraelgnosis@gmail.com>
2020-03-31 02:06:23 -0400

    Move username validation client-side

--- dem3_multi/static/scripts/auth.js
+++ dem3_multi/static/scripts/auth.js
@@ -1,14 +1,28 @@
 $(document).ready(function() {
-    $('input#password-check').on('input', function() {
+    $('input#username').focusout(function() {
+        let username = $(this).val();
+        let taken = usernames.includes(username);
+        if (taken) {
+            $('#error-registration-username').html("Username is already taken");
+        }
+        else {
+            $('#error-registration-username').html(null);
+        }
+    });
+    
+    $('input#password-check').focusout(function() {
         let password = $('input#password');
         console.log(password.val());
         let password_check = $(this);
         console.log(password_check.val());
 
+        $('#error-registration-password').html(null);
+
         if (password.val() == password_check.val()) {
             password_check.css({'background-color': 'lightgreen'});
         } else {
             password_check.css({'background-color': 'brown'});
+            $('#error-registration-password').html("Passwords don't match.")
         }
     });
 });
