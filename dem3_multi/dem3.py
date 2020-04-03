c033cd0e442e88840a8e791a1d264f9bbcfa80cb
azraelgnosis <azraelgnosis@gmail.com>
2020-04-02 23:55:33 -0400

    Add dashboard view

--- dem3_multi/dem3.py
+++ dem3_multi/dem3.py
@@ -7,3 +7,7 @@ bp = Blueprint('dem3', __name__)
 @bp.route('/')
 def index():
     return render_template('index.html')
+
+@bp.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
