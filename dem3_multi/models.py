07188df34143126be83eefccff86c42b36835a16
azraelgnosis <azraelgnosis@gmail.com>
2020-04-02 23:56:17 -0400

    Add from_row() factory to user model

--- dem3_multi/models.py
+++ dem3_multi/models.py
@@ -1,4 +1,5 @@
 import sqlite3
+
 class Row(sqlite3.Row):
     def __init__(self, cursor, values):
         self.cursor = cursor
@@ -11,14 +12,24 @@ class Row(sqlite3.Row):
     def __repr__(self): return ", ".join([f"{key}: {self[key]}" for key in self.keys()])
 
 class Model:
-    pass
+    @classmethod
+    def from_row(cls, row:Row):
+        new_obj = cls()
 
-class Player:
-    __slots__ = ["username", "password", "games"]
+        for key in row.keys():
+            setattr(new_obj, key, row[key]) #? Do Row objects have __getitem__?
 
-    def __init__(self, username, password):
+class User(Model):
+    __slots__ = ["id", "username", "password", "games"]
+
+    def __init__(self, id, username, password):
+        self.id = id
         self.username = username
         self.password = password
+
+    @staticmethod
+    def from_row(row:Row) -> 'User':
+        return User(row.id, row.username, row.password)
 
     def __repr__(self): return f"{self.username}"
         
@@ -27,4 +38,4 @@ class Character:
     __slots__ = ["name", "game"]
 
 class Game(Model):
-    __slots__ = ["name", "players"]
+    __slots__ = ["name", "users"]
