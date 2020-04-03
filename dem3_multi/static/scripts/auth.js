99180e93c28dbc8b012fdbd4b71528703d563efe
azraelgnosis <azraelgnosis@gmail.com>
2020-04-02 23:34:40 -0400

    Add JS validation for login template

--- dem3_multi/static/scripts/auth.js
+++ dem3_multi/static/scripts/auth.js
@@ -1,36 +1,36 @@
 $(document).ready(function() {
-    let username_valid = false;
-    let password_valid = false;
+    let register_username_valid = false;
+    let register_password_valid = false;
 
-    $('input#username').focusout(function() {
+    $('input#register-username').focusout(function() {
         let username = $(this).val().trim().toLowerCase();
         let taken = usernames.includes(username);
         let short = username.length <= 3;
 
         if (!short && !taken) {
-            username_valid = true;
+            register_username_valid = true;
             $('#error-registration-username').html(null);
         } else {
-            username_valid = false;
+            register_username_valid = false;
 
             if (taken) { $('#error-registration-username').html("Username is already taken."); }
             if (short) { $('#error-registration-username').html("Username must be at least 3 characters long."); }
         }
     });
     
-    $('input#password-check, input#password').focusout(function() {
-        let password = $('input#password');
-        let password_check = $('input#password-check');
+    $('input#register-password-check, input#register-password').focusout(function() {
+        let password = $('input#register-password');
+        let password_check = $('input#register-password-check');
         let match = password.val().trim() == password_check.val().trim()
         let short = password.val().trim().length <= 3
 
         $('#error-registration-password').html(null);
 
         if (!short && match) {
-            password_valid = true;
+            register_password_valid = true;
             password_check.css({'background-color': 'lightgreen'});
         } else {
-            password_valid = false;
+            register_password_valid = false;
             password_check.css({'background-color': 'brown'});
 
             if (short) { $('#error-registration-password').html("Password must be at least 4 characters.\n"); }
@@ -38,11 +38,26 @@ $(document).ready(function() {
         }
     });
 
-    $('input#username, #password, input#password-check').focusout(function() {
-        if (username_valid && password_valid) {
+    $('input#register-username, #register-password, input#register-password-check').focusout(function() {
+        if (register_username_valid && register_password_valid) {
             $('#register').prop('disabled', false);
         } else {
             $('#register').prop('disabled', true);
+        }
+    });
+
+
+    let login_username_valid = false;
+    let login_password_valid = false;
+
+    $('input#login-username, input#login-password').focusout(function() {
+        login_username_valid = ($('input#login-username').val().length > 0) ? true : false;
+        login_password_valid = ($('input#login-password').val().length > 0) ? true : false;
+
+        if (login_username_valid && login_password_valid) {
+            $('input#login[type="submit"]').prop('disabled', false);
+        } else {
+            $('input#login[type="submit"]').prop('disabled', true);
         }
     });
 });
