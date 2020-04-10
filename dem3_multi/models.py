2c467f32815e691c72ce41f9e3de5f5e4f26b1b6
azraelgnosis <azraelgnosis@gmail.com>
2020-04-09 23:14:15 -0400

    Add Table model

--- dem3_multi/models.py
+++ dem3_multi/models.py
@@ -25,13 +25,57 @@ class Model:
 
         return new_obj
 
-class User(Model):
-    __slots__ = ["id", "username", "password", "games"]
+class Table(Model):
+    __slots__ = ["db", "name", "columns", "size"]
 
-    def __init__(self, id, username, password):
+    def __init__(self, name:str, db=None):
+        self.db = db
+        self.name = name
+
+        self.columns = []
+        self.size = -1
+        
+        self._set_columns()
+        self._set_size()
+
+    def set_db(self, db:sqlite3.Connection) -> None:
+        self.db = db
+
+    def _set_columns(self) -> None:
+        """
+        Retrieves the columns from the database and assigns them to `self.columns` as a list.
+        """
+        cursor = self.db.execute(f"SELECT * FROM {self.name}")
+        self.columns = [col[0] for col in cursor.description]
+
+    def _set_size(self) -> None:
+        """
+        Retrieves number of rows from database and assigns that to `self.size`
+        """
+
+        size = self.db.execute(
+                f"SELECT COUNT(*) AS count FROM {self.name}"
+            ).fetchone()['count']
+        self.size = size
+
+    def __len__(self): return self.size
+    def __repr__(self): return self.name
+
+class User(Model):
+    __slots__ = ["id", "username", "password", "roles", "games"]
+
+    def __init__(self, id, username, password, roles=set(), games=set()):
         self.id = id
         self.username = username
         self.password = password
+        self.roles = roles
+        self.games = games
+
+    def add_role(self, role:str):
+        self.roles.add(role)
+
+    def add_game(self, game:str):
+        self.games.add(game)
 
     @staticmethod
     def from_row(row:Row) -> 'User':
