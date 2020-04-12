a9328a8d3325437e9562aba43bf572ddbdc7569d
azraelgnosis <azraelgnosis@gmail.com>
2020-04-12 02:44:00 -0400

    Add situation model, view, template

--- dem3_multi/dem3.py
+++ dem3_multi/dem3.py
@@ -3,7 +3,7 @@ from flask import (
 )
 
 from dem3_multi.db import select
-from dem3_multi.data import get_policies, get_policy
+from dem3_multi.data import get_game_data, get_game_datum
 
 bp = Blueprint('dem3', __name__)
 
@@ -27,10 +27,20 @@ def game(id:int):
 
 @bp.route('/policies')
 def policies():
-    policies = get_policies()
+    policies = get_game_data('policies') #policies = get_policies()
     return render_template('policies.html', policies=policies)
 
 @bp.route('/policies/<string:policy_name>')
 def policy(policy_name:str):
-    policy = get_policy(policy_name)
+    policy = get_game_datum('policies', policy_name) # policy = get_policy(policy_name)
     return render_template('policy.html', policy=policy)
+
+@bp.route('/situations/')
+def situations():
+    situations = get_game_data('situations')
+    return render_template('situations.html', situations=situations)
+
+@bp.route('/situations/<string:name>')
+def situation(name:str):
+    situation = get_game_datum('situations', name)
+    return render_template('situation.html', situaiton=situation)
--- dem3_multi/game_models.py
+++ dem3_multi/game_models.py
@@ -3,6 +3,16 @@ from xml.etree.ElementTree import Element
 
 class Game_Model(Model):
     lists = ()
+    #? Might be a problem when it's time to write to file
+    mapping = {
+        'imp': 'implementation',
+        'targ': 'target',
+        'val': 'value',
+        'activehistory': 'active_history',
+        'incom_mult': 'income_mult',
+        'costhistory': 'cost_history',
+        'incomehistory': 'income_history'
+    }
 
     def _split_lists(self):
         for prop in self.lists:
@@ -12,6 +22,13 @@ class Game_Model(Model):
     @classmethod
     def from_xml(cls, xml:Element):
         new_model = cls()
+
+        for tag in list(xml):
+            tag_name = tag.tag
+            tag_text = Game_Model._coerce_type(tag.text)
+            setattr(new_model, Game_Model.mapping.get(tag_name, tag_name), tag_text)
+        
+        new_model._split_lists()
         
         return new_model
 
@@ -24,51 +41,83 @@ class Game_Model(Model):
         """
         split_list = []
 
-        try:
-            split_list = [int(val) for val in prop.split(',')[:-1]]
-        except ValueError:
-            split_list = [float(val) for val in prop.split(',')[:-1]]
+        #? maybe the values should remain as strings
+        split_list = [Game_Model._coerce_type(val) for val in prop.split(',')[:-1]]
+        # try:
+        #     split_list = [int(val) for val in prop.split(',')[:-1]]
+        # except ValueError:
+        #     split_list = [float(val) for val in prop.split(',')[:-1]]
 
         return split_list
 
+    @staticmethod
+    def _coerce_type(val):
+        try:
+            val = int(val)
+        except ValueError:
+            try:
+                val = float(val)
+            except ValueError: 
+                pass
+        return val
+
 
 class Party(Game_Model): ...
-
-class Voter(Game_Model):...
-    
+class Voter(Game_Model): ...
+class VoterType(Game_Model): ...    
 
 class Policy(Game_Model):
     __slots__ = [
-        'name', 'implementation', 'value', 'history', 'active_history', 'active', 'cost_mult', 'income_mult', 'cost_history', 'income_history', 'earn_scalar', 'cost_scalar',
+        'name', 'descripion', 'implementation', 'value', 'history', 'active_history', 'active', 'cost_mult', 'income_mult', 'cost_history', 'income_history', 'earn_scalar', 'cost_scalar',
         'department'    
     ]
     tags = ('name', 'imp', 'targ', 'val', 'history', 'activehistory', 'active', 'cost_mult', 'incom_mult', 'costhistory', 'incomehistory', 'earn_scalar', 'cost_scalar')
     lists = ('history', 'active_history', 'cost_history', 'income_history')
 
-    mapping = {
-        'imp': 'implementation',
-        'targ': 'target',
-        'val': 'value',
-        'activehistory': 'active_history',
-        'incom_mult': 'income_mult',
-        'costhistory': 'cost_history',
-        'incomehistory': 'income_history'
-    }
+    
 
-    # def _split_lists(self):
-    #     self.history = super._split_list(self.history, float) # [float(num) for num in self.history.split(',')]
-    #     self.active_history = super._split_list(self.active_history) # [int(num) for num in self.active_history.split(',')]
-    #     self.cost_history = super._split_list(self.cost_history, float) # [float(num) for num in self.cost_history.split(',')]
-    #     self.income_history = super._split_list(self.income_history, float)
+    def __init__(self):
+        self.name = None
 
-    @staticmethod
-    def from_xml(xml:Element) -> 'Policy':
-        new_policy = Policy()
+    # @staticmethod
+    # def from_xml(xml:Element) -> 'Policy':
+    #     new_policy = Policy()
 
-        for tag in list(xml):
-            tag_name = tag.tag
-            setattr(new_policy, Policy.mapping.get(tag_name, tag_name), tag.text)
+    #     for tag in list(xml):
+    #         tag_name = tag.tag
+    #         tag_text = tag.text
+            
 
-        new_policy._split_lists()
+    #         setattr(new_policy, super.mapping.get(tag_name, tag_name), tag_text)
 
-        return new_policy
+    #     new_policy._split_lists()
+
+    #     return new_policy
+
+    def __repr__(self): return f"{self.name}"
+
+class Situation(Game_Model):
+    lists = ()
+#     @staticmethod
+#     def from_xml(xml:Element) -> 'Situation':
+#         new_situation = Situation()
+
+#         for tag in list(xml):
+#             tag_name = tag.tag
+#             tag_text = super._coerce_type(tag.text)
+
+#             setattr(new_situation, super.mapping.get(tag_name, tag_name), tag_text)
+
+#             new_situation._split_lists()
+
+#             return new_situation
+
+
+class Dilemma(Game_Model): ...
+class Simvalue(Game_Model): ...
+class Grudge(Game_Model): ...
+class Minister(Game_Model): ...
+class PressureGroup(Game_Model): ...
+
+class Save(Game_Model):
+    __slots__ = ["filename", "policies"]
